05c7779ccd0b56a28581542cca3a42094f4f4a6f
Nueng <5f9ae699-600e-439d-b151-5cc403efb5dc-210574356787471281_dsdev@noreply.codeup.aliyun.com>
2026-04-30 15:12:22 +0700

    fix: bind api server to render port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -18,7 +18,11 @@ async function bootstrap() {
         }),
     );
 
-    await app.listen(process.env.PORT ? Number(process.env.PORT) : 3001);
+    const port = Number(process.env.PORT) || 3001;
+
+    await app.listen(port, '0.0.0.0');
+
+    console.log(`API is running on port ${port}`);
 }
 
 bootstrap();
